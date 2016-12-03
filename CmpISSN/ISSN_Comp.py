5580a1c6f10e5e7f615d26706ea355c7b9fdc28f
kanrakun <wwilly0326tw@gmail.com>
2016-12-03 14:55:23 +0800

    變更成從DB取資料

--- CmpISSN/ISSN_Comp.py
+++ CmpISSN/ISSN_Comp.py
@@ -1,9 +1,14 @@
-# -*-coding: <utf-8-*-
+# -*-coding: utf-8-*-
 import sys
 import time
+import Program.DBconfig as DBconfig
+import mysql.connector
+import logging
+import logging.config
 from termcolor.termcolor import colored
 from openpyxl import Workbook
 from openpyxl import load_workbook
+
 
 PISSN = []
 EISSN = []
@@ -28,85 +33,26 @@ def getISSN():
         EISSN.append(row[0].value)
     print(colored('Reading SFX-ISSN... ', 'blue', attrs=['bold', 'underline']) + colored('Completed.', 'yellow'))
 
+
 """比對PISSN EISSN 是否有相同的，若相同回傳其index"""
 
 
 def cmpISSN(ISSN):
-    index = -1
+
     try:
-        index = EISSN.index(ISSN)
-    except ValueError:
-        try:
-            index = PISSN.index(ISSN)
-        except ValueError:
-            index = -1
+        conn = mysql.connector.connect(user=DBconfig.user, password=DBconfig.password, database=DBconfig.database,
+                                       host=DBconfig.host)
+        cur = conn.cursor()
+    except Exception as err:
+        logger.error(err)
+        return False
 
-    if index is not -1:
-        print (colored('Match ISSN in index :  ', 'blue', attrs=['bold', 'underline']) + colored(str(index), 'yellow'))
-        return index
-
-
-# def cmpISSN():
-#     count = 0
-#     totalRowIndex = 1
-#     startTime = time.time()
-#
-#     """This output is Matched the ISSN"""
-#     outputwb = Workbook()
-#     outputws = outputwb[outputwb.sheetnames[0]]
-#     outputData = []
-#     outputws.append(header)
-#     outputwb.save('./resultData/MatchISSN.xlsx')
-#
-#     """This output is Not Matched the ISSN"""
-#     outputwb2 = Workbook()
-#     outputws2 = outputwb2[outputwb2.sheetnames[0]]
-#     outputData2 = []
-#     outputws2.append(header)
-#     outputwb2.save('./resultData/noMatchISSN.xlsx')
-#
-#     for year in range(2016, 2017):
-#         print('Processing ' + str(year) + " year's file.")
-#         wb = load_workbook(filename='Data/Year/' + str(year) + '.xlsx', read_only=True)
-#         ws = wb[wb.sheetnames[0]]
-#         rowIndex = 1
-#         ISSN_str = 'Q2:Q' + str(ws.max_row)
-#
-#         for row in ws[ISSN_str]:
-#             totalRowIndex += 1
-#             rowIndex += 1
-#             if row[0].value is None:
-#                 continue
-#             if totalRowIndex % 1000 is 0:
-#                 print('Total processed count : ' + str(totalRowIndex))
-#                 endTime = time.time()
-#                 print('Time consuming : ' + str(endTime - startTime))
-#                 startTime = time.time()
-#
-#             elif row[0].value in PISSN or row[0].value in EISSN:
-#                 count += 1
-#
-#                 for one_row in ws.iter_rows(str(rowIndex)):
-#                     for col in one_row:
-#                         outputData.append(col.value)
-#                 nRow = outputws.max_row + 1
-#                 for i in range(1, len(outputData) + 1):
-#                     outputws.cell(row=nRow, column=i).value = outputData[i - 1]
-#                 outputData = []
-#
-#             else:
-#                 for one_row in ws.iter_rows(str(rowIndex)):
-#                     for col in one_row:
-#                         outputData2.append(col.value)
-#                 nRow = outputws2.max_row + 1
-#                 for i in range(1, len(outputData2) + 1):
-#                     outputws2.cell(row=nRow, column=i).value = outputData2[i - 1]
-#                 outputData2 = []
-#
-#         print('Match count : ' + str(count))
-#         print('Saving files.')
-#         outputwb.save('./resultData/MatchISSN.xlsx')
-#         outputwb2.save('./resultData/noMatchISSN.xlsx')
+    try:
+        cur.execute("SELECT id from sfx where ISSN = '" + ISSN + "'")
+        return cur.fetchall()
+    except Exception as err:
+        logger.error(err)
+        return None
 
 
 def traceback(err):
@@ -116,4 +62,6 @@ def traceback(err):
 
 
 if __name__ == '__main__':
-    print (cmpISSN('0033-1236'))
+    logging.config.fileConfig("../logger.conf")
+    logger = logging.getLogger("root")
+    print(cmpISSN('0256-1891'))
