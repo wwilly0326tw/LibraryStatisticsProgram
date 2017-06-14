4764f9599f98f0198ecece8b591346f694b86308
kanrakun <wwilly0326tw@gmail.com>
2017-06-14 15:16:39 +0800

    main 院系對應資料取出方式修改

--- DumpData/Dump.py
+++ DumpData/Dump.py
@@ -2,10 +2,22 @@ from openpyxl import load_workbook
 import mysql.connector
 import DBconfig as DBconfig
 
+conn = mysql.connector.connect(user=DBconfig.user, password=DBconfig.password, database=DBconfig.database,
+                               host=DBconfig.host)
+cur = conn.cursor()
+
 
 def dump():
-    wb = load_workbook(filename="scopus.xlsx")
+    wb = load_workbook(filename="1.xlsx")
     ws = wb[wb.sheetnames[0]]
+    dataStr = 'A2:A' + str(ws.max_row)
+    for row in ws[dataStr]:
+        scopusID = row[0].value
+        cur.execute("Select sfxID from support where scopusID = " + str(scopusID))
+        sfxIDList = cur.fetchall()
+        if sfxIDList is not None and len(sfxIDList) is not 0:
+            for id in sfxIDList:
+                print (id)
     return
 if __name__ == '__main__':
     # main(filename="../Data/scopus/scopus.xlsx")
