6d614d3c8ca7b0b5c78391d87aed3232bbcf99c4
kanrakun <wwilly0326tw@gmail.com>
2017-06-21 15:26:55 +0800

    online versuib

--- CmpISSN/ISSN_Comp.py
+++ CmpISSN/ISSN_Comp.py
@@ -8,9 +8,9 @@ import logging
 import logging.config
 
 """Input ISSN 或 ISBN 數組，比較相同的SFX資料回傳"""
-debug = 1
+debug = 0
 
-def cmpISSNISBN(ISSN="00189294", ISBN="", year="2017"):
+def cmpISSNISBN(ISSN="", ISBN="", year=""):
     logger = logging.getLogger("root")
     try:
         conn = mysql.connector.connect(user=DBconfig.user, password=DBconfig.password, database=DBconfig.database,
@@ -25,7 +25,8 @@ def cmpISSNISBN(ISSN="00189294", ISBN="", year="2017"):
         if ISSN.find(";") is not -1:
             ISSNstr = split(ISSN=ISSN)
         try:
-            stmt = "SELECT id from sfx where year = " + str(year) + " and ISSN in ('" + ISSNstr + "') or eISSN in ('" + ISSNstr + "')"
+            stmt = "SELECT id from sfx where year = " + str(
+                year) + " and ISSN in ('" + ISSNstr + "') or eISSN in ('" + ISSNstr + "')"
             if debug:
                 print (stmt)
             cur.execute(stmt)
