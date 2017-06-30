9bccf0f8cd1b6d9b6f85da02343ee8bdde9fbc44
kanrakun <wwilly0326tw@gmail.com>
2017-06-30 14:44:50 +0800

    Write Department

--- online/python/WriteDB/writeDepartment.py
+++ online/python/WriteDB/writeDepartment.py
@@ -1,3 +1,4 @@
+from openpyxl import load_workbook
 import DBconfig as DBconfig
 import mysql.connector
 import logging
@@ -7,18 +8,67 @@ import logging.config
 """ 程式用途為更新Department Table """
 """ 查資料庫中存不存在，不存在就往後加上去，要對應到院 """
 
+debug = 1
+logging.config.fileConfig("../logger.conf")
+logger = logging.getLogger("root")
+try:
+    conn = mysql.connector.connect(user=DBconfig.user, password=DBconfig.password, database=DBconfig.database,
+                                   host=DBconfig.host)
+    cur = conn.cursor()
+except Exception as err:
+    logger.error(err)
+
 def writeDepartment(filename = ""):
     if filename == "":
         return False
-    logging.config.fileConfig("../python/logger.conf")
-    logger = logging.getLogger("root")
+
+    wb = load_workbook(filename=filename)
+    ws = wb[wb.sheetnames[0]]
+
+    for row in ws['A2:D' + str(ws.max_row)]:
+        depName = row[3].value
+        if not checkIfExist(depName=depName):
+            colName = row[1].value
+            if not checkIfExist(colName=colName):
+                stmt = "Insert into college(name) values(\"" + colName + "\")"
+                if debug:
+                    print (stmt)
+                try:
+                    cur.execute(stmt)
+                    conn.commit()
+                except Exception as err:
+                    logger.error(err)
+            stmt = "Select cid from college where name = \"" + colName + "\""
+            if debug:
+                print (stmt)
+            try:
+                cur.execute(stmt)
+                cid = cur.fetchone()[0]
+            except Exception as err:
+                logger.error(err)
+
+            stmt = "Insert into department(name,cid) values(\"" + depName + "\", " + str(cid) + ")"
+            if debug:
+                print (stmt)
+            try:
+                cur.execute(stmt)
+                conn.commit()
+            except Exception as err:
+                logger.error(err)
+
+def checkIfExist(depName="", colName=""):
+    if depName is not "":
+        stmt = "Select count(*) from department where name = \"" + depName + "\""
+    elif colName is not "":
+        stmt = "Select count(*) from college where name = \"" + colName + "\""
+    if debug:
+        print (stmt)
     try:
-        conn = mysql.connector.connect(user=DBconfig.user, password=DBconfig.password, database=DBconfig.database,
-                                       host=DBconfig.host)
-        cur = conn.cursor()
+        cur.execute(stmt)
+        return cur.fetchone()[0]
     except Exception as err:
         logger.error(err)
-        return False
-
+        return
 if __name__ == '__main__':
-    writeDepartment()
+    # writeDepartment(filename="./院系表20170630.xls")
+    writeDepartment(filename="./testdata.xlsx")
