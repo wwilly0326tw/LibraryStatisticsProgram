469b9ac1a14d3610d4fc465ea927c6abf56608c3
kanrakun <wwilly0326tw@gmail.com>
2017-06-30 10:49:53 +0800

    插入sfx 時自動加入新的target

--- WriteDB/writeSFXtoDB.py
+++ WriteDB/writeSFXtoDB.py
@@ -4,25 +4,23 @@ import mysql.connector
 import logging
 import logging.config
 
-debug = 1
+debug = 0
+
+logging.config.fileConfig("../logger.conf")
+logger = logging.getLogger("root")
+
+try:
+    conn = mysql.connector.connect(user=DBconfig.user, password=DBconfig.password, database=DBconfig.database,
+                                   host=DBconfig.host)
+    cur = conn.cursor()
+except Exception as err:
+    logger.error(err)
 
 """ 此程式用於將sfx資料寫入資料庫中 """
 def writeSFX2DB(filename="", year=""):
     if filename is "":
         print ("Please Input a file.")
         return False
-
-    logging.config.fileConfig("../logger.conf")
-    logger = logging.getLogger("root")
-
-    try:
-        conn = mysql.connector.connect(user=DBconfig.user, password=DBconfig.password, database=DBconfig.database,
-                                       host=DBconfig.host)
-        cur = conn.cursor()
-    except Exception as err:
-        logger.error(err)
-        return False
-
     sqlStmt = "insert into sfx(SortableTitle,Title,TitleNon‐FilingCharacter,ISSN,ObjectID,TargetPublicName,Threshold,Eissn,AbbreviatedTitle,TargetServiceType,LCCN,ObjectPortfolioID,856‐u,856‐y,856‐a,245_h,LocalThreshold,GlobalThreshold,TargetID,TargetServiceID,ObjectPortfolio_ID,Categories,LocalAttribute,ISBN,eISBN,Publisher,PlaceofPublication,DateofPublication,ObjectType,ActivationstatusfortheDEFAULTinstitute,InstituteID,InstituteName,InstituteAvailability,Language,MainTitle,FullOriginalTitle,AdditionalISBNs,AdditionaleISBNs,Author,Owner,THRESHOLD_LOCAL, isFree, year) values ("
 
     wb = load_workbook(filename=filename)
@@ -34,10 +32,11 @@ def writeSFX2DB(filename="", year=""):
         if ISSN is not None:
             ISSN = ISSN.replace("-", "")
             row[3].value = ISSN
-        eISSN = row[7].value
+            eISSN = row[7].value
         if eISSN is not None:
             eISSN = eISSN.replace("-", "")
             row[7].value = eISSN
+        checkTargetinDB(row[5].value)
         for col in row:
             if col.value is None:
                 valStr += 'NULL'
@@ -65,8 +64,24 @@ def writeSFX2DB(filename="", year=""):
             conn.rollback()
             continue
 
+def checkTargetinDB(target):
+    stmt = "Select count(*) from target  where name = \"" + target + "\""
+    try:
+        if debug:
+            print (stmt)
+        cur.execute(stmt)
+        exist = cur.fetchone()
+        if not exist[0]:
+            stmt = "Insert into target(name) values(\"" + target + "\")"
+            if debug:
+                print (stmt)
+            cur.execute(stmt)
+    except Exception as err:
+        print (err)
+        logger.error(err)
+        conn.rollback()
 
 if __name__ == '__main__':
-    writeSFX2DB('../../Data/SFX/testsfx.xlsx', "2018")
-
+    # writeSFX2DB('../../Data/SFX/testsfx.xlsx', "2018")
+    writeSFX2DB('./testdata.xlsx', "2018")
 
--- online/python/WriteDB/writeSFXtoDB.py
+++ online/python/WriteDB/writeSFXtoDB.py
@@ -6,22 +6,21 @@ import logging
 import logging.config
 
 debug = 0
+filename = "../data/" + filename
+logging.config.fileConfig("../python/logger.conf")
+logger = logging.getLogger("root")
+
+try:
+    conn = mysql.connector.connect(user=DBconfig.user, password=DBconfig.password, database=DBconfig.database,
+                                   host=DBconfig.host)
+    cur = conn.cursor()
+except Exception as err:
+    logger.error(err)
 
 """ 此程式用於將sfx資料寫入資料庫中 """
 def writeSFX2DB(filename="", year=""):
     if filename is "":
         print ("Please Input a file.")
-        return False
-    filename = "../data/" + filename
-    logging.config.fileConfig("../python/logger.conf")
-    logger = logging.getLogger("root")
-
-    try:
-        conn = mysql.connector.connect(user=DBconfig.user, password=DBconfig.password, database=DBconfig.database,
-                                       host=DBconfig.host)
-        cur = conn.cursor()
-    except Exception as err:
-        logger.error(err)
         return False
 
     sqlStmt = "insert into sfx(SortableTitle,Title,TitleNon‐FilingCharacter,ISSN,ObjectID,TargetPublicName,Threshold,Eissn,AbbreviatedTitle,TargetServiceType,LCCN,ObjectPortfolioID,856‐u,856‐y,856‐a,245_h,LocalThreshold,GlobalThreshold,TargetID,TargetServiceID,ObjectPortfolio_ID,Categories,LocalAttribute,ISBN,eISBN,Publisher,PlaceofPublication,DateofPublication,ObjectType,ActivationstatusfortheDEFAULTinstitute,InstituteID,InstituteName,InstituteAvailability,Language,MainTitle,FullOriginalTitle,AdditionalISBNs,AdditionaleISBNs,Author,Owner,THRESHOLD_LOCAL, isFree, year) values ("
@@ -32,11 +31,14 @@ def writeSFX2DB(filename="", year=""):
     for row in ws['A2:AO' + str(ws.max_row)]:
         valStr = ""
         ISSN = row[3].value
-        ISSN = ISSN.replace("-", "")
-        row[3].value = ISSN
+        if ISSN is not None:
+            ISSN = ISSN.replace("-", "")
+            row[3].value = ISSN
         eISSN = row[7].value
-        eISSN = eISSN.replace("-", "")
-        row[7].value = eISSN
+        if eISSN is not None:
+            eISSN = eISSN.replace("-", "")
+            row[7].value = eISSN
+        checkTargetinDB(row[5].value)
         for col in row:
             if col.value is None:
                 valStr += 'NULL'
@@ -64,6 +66,23 @@ def writeSFX2DB(filename="", year=""):
             logger.error(err)
             conn.rollback()
             continue
+
+def checkTargetinDB(target):
+    stmt = "Select count(*) from target  where name = \"" + target + "\""
+    try:
+        if debug:
+            print (stmt)
+        cur.execute(stmt)
+        exist = cur.fetchone()
+        if not exist[0]:
+            stmt = "Insert into target(name) values(\"" + target + "\")"
+            if debug:
+                print (stmt)
+            cur.execute(stmt)
+    except Exception as err:
+        print (err)
+        logger.error(err)
+        conn.rollback()
 
 if __name__ == '__main__':
     writeSFX2DB('./testdata.xlsx', 2015)
